a296ec1ea2f1304f5341ca4c184525dabe4717ee
Galuh Pradipta <galuh.pradipta@brank.as>
2023-03-08 21:53:45 +0700

    add functionality

--- go.mod
+++ go.mod
@@ -9,6 +9,8 @@ require (
 	github.com/gofiber/utils v0.0.10 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/schema v1.1.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
@@ -17,6 +19,7 @@ require (
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/savsgio/dictpool v0.0.0-20221023140959-7bf2e61cea94 // indirect
 	github.com/savsgio/gotils v0.0.0-20220530130905-52f3993e8d6d // indirect
+	github.com/slack-go/slack v0.12.1 // indirect
 	github.com/tinylib/msgp v1.1.6 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.44.0 // indirect
--- main.go
+++ main.go
@@ -1,13 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"github.com/slack-go/slack"
 )
 
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+}
+
+var (
+	slackConn    *slack.Client
+	slackChannel = ""
+)
+
+func createSlackConn(token string) {
+	slackToken := os.Getenv("SLACK_TOKEN")
+	if slackToken == "" {
+		log.Fatal("SLACK_TOKEN is not set")
+	}
+	slackConn = slack.New(token)
+}
+
 func main() {
+	slackToken := os.Getenv("SLACK_TOKEN")
+	if slackToken == "" {
+		log.Fatal("SLACK_TOKEN is not set")
+	}
+
+	slackChannel = os.Getenv("SLACK_CHANNEL")
+	if slackChannel == "" {
+		log.Fatal("SLACK_CHANNEL is not set")
+	}
+	createSlackConn(slackToken)
+
 	app := fiber.New()
 	app.Post("/alert", sendAlert)
 	log.Fatal(app.Listen(":3000"))
@@ -20,6 +55,17 @@ func sendAlert(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{
 			"message": err.Error(),
 		})
+	}
+
+	if req.Type == "SpamNotification" {
+		_, _, _, err := slackConn.SendMessage(slackChannel,
+			slack.MsgOptionText(fmt.Sprintf("%s, Email: %s", req.Description, req.Email), false))
+		if err != nil {
+			c.Status(fiber.StatusInternalServerError)
+			return c.JSON(fiber.Map{
+				"message": err.Error(),
+			})
+		}
 	}
 
 	return c.JSON(fiber.Map{
